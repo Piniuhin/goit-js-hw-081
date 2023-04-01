75c640abfc39bd8bb31e4e6710b01a26799ae650
Alexander Piniuhin <85253534+Piniuhin@users.noreply.github.com>
2023-04-01 12:59:54 +0200

    corrected

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,6 +5,31 @@ import { galleryItems } from './gallery-items';
 // Описан в документации
 import SimpleLightbox from 'simplelightbox';
 // Дополнительный импорт стилей
-// import 'simplelightbox/dist/simple-lightbox.min.css';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(SimpleLightbox);
+const gallaryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItems(galleryItems);
+gallaryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+gallaryContainer.addEventListener('click', onImgClick);
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    )
+    .join('');
+}
+function onImgClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+// console.log(SimpleLightbox);
