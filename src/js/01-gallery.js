514d9ecd3da55db459eb678421fe6e5dd7b6f472
Alexander Piniuhin <85253534+Piniuhin@users.noreply.github.com>
2023-04-01 12:05:03 +0200

    add simple

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,10 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+// Change code below this line
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+console.log(SimpleLightbox);
